b51b87f93860c28d00bb8cadc4606c23996ea71e
nerk <kern99@web.de>
2015-01-24 19:21:23 +0100

    Always use globally installed jvm, ignore JAVA_HOME

--- src/main/scripts/gpxtcx.cmd
+++ src/main/scripts/gpxtcx.cmd
@@ -7,11 +7,7 @@ setlocal EnableDelayedExpansion EnableExtensions
 
 set JAVA_EXE=java.exe
 
-If defined JAVA_HOME (
-    set JAVA_EXE=%JAVA_HOME%\bin\%JAVA_EXE%
-) 
-
-set VM_OPTS=-Dfile.encoding=utf-8 -Xmx768m -Xverify:none -XX:+TieredCompilation -XX:TieredStopAtLevel=1
+set VM_OPTS=-Dfile.encoding=utf-8
 
 set DIR=%~dp0
 set LIBDIR=%DIR%lib
